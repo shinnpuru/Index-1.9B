e87938b037080570e96b71ad2b2b3d2b7ee3d4c7
ryanhe312 <89232015+shinnpuru@users.noreply.github.com>
2024-08-04 23:13:05 +0800

    add save

--- roleplay/hf_based_demo_gguf.py
+++ roleplay/hf_based_demo_gguf.py
@@ -30,7 +30,7 @@ llm = Llama(model_path = gguf_model_path, n_gpu_layers=-1, verbose=True, n_ctx=0
 # model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16,trust_remote_code=True)
 
 
-def generate_with_question(question, role_name):
+def generate_with_question(question, role_name, max_seq_len=2048):
     question_in = "\n".join(["\n".join(pair) for pair in question])
 
     g = GetManualTestSamples(
@@ -39,7 +39,7 @@ def generate_with_question(question, role_name):
         save_samples_dir="./character",
         save_samples_path=role_name + "_rag.json",
         prompt_path="./prompt/dataset_character.txt",
-        max_seq_len=4000
+        max_seq_len=max_seq_len
     )
     g.get_qa_samples_by_query(
         questions_query=question_in,
@@ -61,8 +61,8 @@ def create_datasets(role_name):
     save_to_json(testset, f"./character/{role_name}_测试问题.json")
 
 
-def hf_gen(dialog: List, role_name, top_k, top_p, temperature, repetition_penalty, max_dec_len):
-    generate_with_question(dialog, role_name)
+def hf_gen(dialog: List, role_name, top_k, top_p, temperature, repetition_penalty, max_dec_len, max_seq_len):
+    generate_with_question(dialog, role_name, max_seq_len)
     create_datasets(role_name)
 
     json_data = load_json(f"{character_path}/{role_name}_测试问题.json")[0]
@@ -106,7 +106,7 @@ def hf_gen(dialog: List, role_name, top_k, top_p, temperature, repetition_penalt
         yield answer  # 返回新生成的文本
 
 
-def generate(chat_history: List, query, role_name, role_desc, top_k, top_p, temperature, repetition_penalty, max_dec_len):
+def generate(chat_history: List, query, role_name, user_name, top_k, top_p, temperature, repetition_penalty, max_dec_len, max_seq_len):
     """generate after hitting "submit" button
 
     Args:
@@ -121,13 +121,13 @@ def generate(chat_history: List, query, role_name, role_desc, top_k, top_p, temp
     """
     assert query != "", "Input must not be empty!!!"
     # apply chat template
-    chat_history.append([f"user:{query}", ""])
-    for answer in hf_gen(chat_history, role_name, top_k, top_p, temperature, repetition_penalty, max_dec_len):
+    chat_history.append([f"{user_name}:{query}", ""])
+    for answer in hf_gen(chat_history, role_name, top_k, top_p, temperature, repetition_penalty, max_dec_len, max_seq_len):
         chat_history[-1][1] = role_name + ":" + answer
         yield gr.update(value=""), chat_history
 
 
-def regenerate(chat_history: List,role_name,role_desc, top_k, top_p, temperature, repetition_penalty, max_dec_len):
+def regenerate(chat_history: List,role_name,role_desc, top_k, top_p, temperature, repetition_penalty, max_dec_len, max_seq_len):
     """re-generate the answer of last round's query
 
     Args:
@@ -143,7 +143,7 @@ def regenerate(chat_history: List,role_name,role_desc, top_k, top_p, temperature
     if len(chat_history[-1]) > 1:
         chat_history[-1][1] = ""
     # apply chat template
-    for answer in hf_gen(chat_history, role_name, top_k, top_p, temperature, repetition_penalty, max_dec_len):
+    for answer in hf_gen(chat_history, role_name, top_k, top_p, temperature, repetition_penalty, max_dec_len, max_seq_len):
         chat_history[-1][1] = role_name + ":" + answer
         yield gr.update(value=""), chat_history
 
@@ -157,6 +157,25 @@ def clear_history():
     torch.cuda.empty_cache()
     return []
 
+def save_history(chat_history: List, role_name):
+    """save chat history to local file
+
+    Args:
+        chat_history (List): [[q_1, a_1], [q_2, a_2], ..., [q_n, a_n]]. list that stores all QA records
+        role_name (str): role name
+
+    Returns:
+        List: chat history
+    """
+    role_data_path = os.path.join("./character", role_name + ".json")
+    role_data = load_json(role_data_path)
+
+    for chat in chat_history:
+        role_data.append(role_data[0].copy())
+        role_data[-1]["dialog"] = chat
+    save_to_json(role_data, role_data_path)
+    gr.Info(f"{role_name}对话保存成功")
+    
 
 def generate_file(file_obj, role_info):
     role_name = os.path.basename(file_obj).split(".")[0]
@@ -171,36 +190,45 @@ def generate_file(file_obj, role_info):
 def main():
     # launch gradio demo
     with gr.Blocks(theme="soft") as demo:
-        gr.Markdown("""# Index-1.9B RolePlay Gradio Demo""")
+        gr.Markdown("""# Index-1.9B 角色扮演演示""")
 
         with gr.Row():
             with gr.Column(scale=1):
-                top_k = gr.Slider(0, 10, value=5, step=1, label="top_k")
-                top_p = gr.Slider(0.0, 1.0, value=0.8, step=0.1, label="top_p")
-                temperature = gr.Slider(0.1, 2.0, value=0.85, step=0.1, label="temperature")
-                repetition_penalty = gr.Slider(0.1, 2.0, value=1.0, step=0.1, label="repetition_penalty")
-                max_dec_len = gr.Slider(1, 4096, value=512, step=1, label="max_dec_len")
-                file_input = gr.File(label="上传角色对话语料(.csv)")
-                role_description = gr.Textbox(label="Role Description", placeholder="输入角色描述", lines=2)
+                gr.Markdown("""## 👼 角色生成""")
+                file_input = gr.File(label="上传角色对话语料(角色名称.csv)")
+                role_description = gr.Textbox(label="角色描述", placeholder="请输入角色描述。", lines=2)
                 upload_button = gr.Button("生成角色!")
                 upload_button.click(generate_file, inputs=[file_input, role_description])
-            with gr.Column(scale=10):
-                chatbot = gr.Chatbot(bubble_full_width=False, height=400, label='Index-1.9B')
-                with gr.Row():
-                    role_name = gr.Textbox(label="Role name", placeholder="Input your rolename here!", lines=2, value="三三")
-                user_input = gr.Textbox(label="User", placeholder="Input your query here!", lines=2)
-                with gr.Row():
-                    submit = gr.Button("🚀 Submit")
-                    clear = gr.Button("🧹 Clear")
-                    regen = gr.Button("🔄 Regenerate")
 
-        submit.click(generate, inputs=[chatbot, user_input, role_name, role_description, top_k, top_p, temperature,
-                                       repetition_penalty, max_dec_len],
+                gr.Markdown("""## 🔧 模型参数""")
+                top_k = gr.Slider(0, 10, value=5, step=1, label="单步采用Token的数量")
+                top_p = gr.Slider(0.0, 1.0, value=0.8, step=0.1, label="单步累计采用阈值")
+                temperature = gr.Slider(0.1, 2.0, value=0.85, step=0.1, label="生成文本随机性（温度）")
+                repetition_penalty = gr.Slider(0.1, 2.0, value=1.0, step=0.1, label="重复惩罚系数")
+                max_dec_len = gr.Slider(1, 4096, value=512, step=1, label="最大输出长度")
+                max_seq_len = gr.Slider(1, 4096, value=2048, step=1, label="最大输入长度")
+
+            with gr.Column(scale=10):
+                gr.Markdown("""## 😀 角色扮演""")
+                chatbot = gr.Chatbot(bubble_full_width=False, height=600, label='Index-1.9B')
+                with gr.Row():
+                    role_name = gr.Textbox(label="聊天角色", placeholder="输入聊天角色名称。", lines=2, value="三三")
+                    user_name = gr.Textbox(label="你的名字", placeholder="输入你的名字。", lines=2, value="主人")
+                user_input = gr.Textbox(label="用户", placeholder="输入你的聊天。", lines=2)
+                with gr.Row():
+                    submit = gr.Button("🚀 提交对话")
+                    clear = gr.Button("🧹 清空对话")
+                    regen = gr.Button("🔄 重新生成")
+                    save = gr.Button("💾 保存对话")
+
+        submit.click(generate, inputs=[chatbot, user_input, role_name, user_name, top_k, top_p, temperature,
+                                       repetition_penalty, max_dec_len, max_seq_len],
                      outputs=[user_input, chatbot])
         regen.click(regenerate,
-                    inputs=[chatbot, role_name, role_description, top_k, top_p, temperature, repetition_penalty,
-                            max_dec_len],
+                    inputs=[chatbot, role_name, user_name, top_k, top_p, temperature, repetition_penalty,
+                            max_dec_len, max_seq_len],
                     outputs=[user_input, chatbot])
+        save.click(save_history, inputs=[chatbot, role_name], outputs=[])
         clear.click(clear_history, inputs=[], outputs=[chatbot])
 
     demo.queue()
